91226b111e1ad02149b57552bd37eaaff6975441
AndreiZernov <andrei.zernov@outlook.com>
2022-08-07 14:15:04 +0100

    feat: filtering

---
+++ src/main/kotlin/Filtering.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val spices = listOf("curry", "pepper", "cayenne", "ginger", "red curry", "green curry", "red pepper" )
+    val curries = spices.filter { it.contains("curry") }
+    val sortedCurries = curries.sorted()
+    val newFilteredSpices = spices.filter { it[0] == 'c' && it[it.length - 1] == 'e' }
+    val firstThree = spices.take(3).filter { it[0] == 'c' }
+    println("curries: $curries")
+    println("sortedCurries: $sortedCurries")
+    println("newFilteredSpices: $newFilteredSpices")
+    println("firstThree: $firstThree")
+}
